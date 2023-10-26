61e15fed8f71899c0c885ee3020f940554d8f739
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2023-10-26 00:05:11 -0300

    removendo rotas desnecessárias

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [FormularioController::class, 'create']);
 Route::post('/form/store', [FormularioController::class, 'store'])->name('form.store');
 Route::get('/validar/{codigo}', [FormularioController::class, 'validar']);
-Route::get('/viewPDF', [FormularioController::class, 'viewPdfTest']);
 Route::get('/pdf', [FormularioController::class, 'pdfTest']);
-Route::get('/generatePDF', [FormularioController::class, 'generatePdfTest']);
